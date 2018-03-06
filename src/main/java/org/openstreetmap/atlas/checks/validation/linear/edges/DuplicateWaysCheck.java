c376b9a60911684d0ab1085b2d2cc5ea05731c4d
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-03-06 13:10:33 -0800

    reasoning some logic behind checking total geometry encapsulation, and partial encapsulation; implement hashmap for fast storage

---
+++ src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/DuplicateWaysCheck.java
@@ -0,0 +1,104 @@
+package org.openstreetmap.atlas.checks.validation.linear.edges;
+
+import java.util.*;
+
+import org.openstreetmap.atlas.checks.base.BaseCheck;
+import org.openstreetmap.atlas.checks.flag.CheckFlag;
+import org.openstreetmap.atlas.geography.PolyLine;
+import org.openstreetmap.atlas.geography.Segment;
+import org.openstreetmap.atlas.geography.atlas.items.AtlasObject;
+import org.openstreetmap.atlas.geography.atlas.items.Edge;
+import org.openstreetmap.atlas.utilities.configuration.Configuration;
+
+/**
+ * Auto generated Check template
+ *
+ * @author savannahostrowski
+ */
+public class DuplicateWaysCheck extends BaseCheck
+{
+
+    // You can use serialver to regenerate the serial UID.
+    private static final long serialVersionUID = 1L;
+    public static final String DUPLICATE_EDGE_INSTRUCTIONS = "This way, {0, number, integer}, has "
+            + "is a duplicate or has a duplicate edge segment.";
+    public static final List<String> FALLBACK_INSRUCTIONS = Arrays.asList(
+           DUPLICATE_EDGE_INSTRUCTIONS);
+
+    public static final Map<Long, List<Segment>> edges = new HashMap<>();
+    public static final Iterator iterator = edges.entrySet().iterator();
+
+    @Override
+    protected List<String> getFallbackInsructions() {
+        return FALLBACK_INSRUCTIONS;
+    }
+
+    /**
+     * The default constructor that must be supplied. The Atlas Checks framework will generate the
+     * checks with this constructor, supplying a configuration that can be used to adjust any
+     * parameters that the check uses during operation.
+     *
+     * @param configuration
+     *            the JSON configuration for this check
+     */
+    public DuplicateWaysCheck(final Configuration configuration)
+    {
+        super(configuration);
+    }
+
+    /**
+     * This function will validate if the supplied atlas object is valid for the check.
+     *
+     * @param object
+     *            the atlas object supplied by the Atlas-Checks framework for evaluation
+     * @return {@code true} if this object should be checked
+     */
+    @Override
+    public boolean validCheckForObject(final AtlasObject object)
+    {
+       return object instanceof Edge && !this.isFlagged(object.getIdentifier());
+    }
+
+    /**
+     * This is the actual function that will check to see whether the object needs to be flagged.
+     *
+     * @param object
+     *            the atlas object supplied by the Atlas-Checks framework for evaluation
+     * @return an optional {@link CheckFlag} object that
+     */
+    @Override
+    protected Optional<CheckFlag> flag(final AtlasObject object)
+    {
+        final Edge edge = (Edge) object;
+        final List<Segment> edgeSegments = edge.asPolyLine().segments();
+
+        // this means that there are two features with the same id (probably shouldn't happen)
+        if (edges.containsKey(edge.getIdentifier())) {
+            //throw flag
+        }
+        else {
+            // check all edges in the map to see if they have the same geometry
+            while (iterator.hasNext()) {
+                // get the current entry's segments, and check that the current edge is not totally
+                // contained by the entry's segments...otherwise flag
+
+                //check that the current entry's segments do not partially encapsulate the geometry
+                // of the the current edge, other wise flag
+
+                //else add the edge id, and geometry to the hashmap
+            }
+
+
+        }
+
+
+
+
+
+
+
+        return Optional.empty();
+    }
+
+
+}
