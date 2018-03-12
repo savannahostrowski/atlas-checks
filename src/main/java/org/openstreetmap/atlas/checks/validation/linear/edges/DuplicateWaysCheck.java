97e782a0c7b40798774ed06f0e4277a11484a582
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-03-12 11:36:48 -0700

    changed logic to use [segment, list of id] hashmap

--- src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/DuplicateWaysCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/linear/edges/DuplicateWaysCheck.java
@@ -25,76 +25,56 @@ public class DuplicateWaysCheck extends BaseCheck
     public static final List<String> FALLBACK_INSRUCTIONS = Arrays.asList(
            DUPLICATE_EDGE_INSTRUCTIONS);
 
-    public static final Map<Long, List<Segment>> edges = new HashMap<>();
-    public static final Iterator iterator = edges.entrySet().iterator();
+    private final Map<Segment, Set<Long>> globalSegments = new HashMap<>();
+
 
     @Override
     protected List<String> getFallbackInsructions() {
         return FALLBACK_INSRUCTIONS;
     }
 
-    /**
-     * The default constructor that must be supplied. The Atlas Checks framework will generate the
-     * checks with this constructor, supplying a configuration that can be used to adjust any
-     * parameters that the check uses during operation.
-     *
-     * @param configuration
-     *            the JSON configuration for this check
-     */
+
     public DuplicateWaysCheck(final Configuration configuration)
     {
         super(configuration);
     }
 
-    /**
-     * This function will validate if the supplied atlas object is valid for the check.
-     *
-     * @param object
-     *            the atlas object supplied by the Atlas-Checks framework for evaluation
-     * @return {@code true} if this object should be checked
-     */
+
     @Override
     public boolean validCheckForObject(final AtlasObject object)
     {
        return object instanceof Edge && !this.isFlagged(object.getIdentifier());
     }
 
-    /**
-     * This is the actual function that will check to see whether the object needs to be flagged.
-     *
-     * @param object
-     *            the atlas object supplied by the Atlas-Checks framework for evaluation
-     * @return an optional {@link CheckFlag} object that
-     */
+
     @Override
     protected Optional<CheckFlag> flag(final AtlasObject object)
     {
-
-        // Cast Atlas object to Edge
         final Edge edge = (Edge) object;
-        // Get Edge as a List of Segments
-        final List<Segment> currentEdgeSegments = edge.asPolyLine().segments();
+        final long identifier = edge.getIdentifier();
+        final List<Segment> edgeSegments = edge.asPolyLine().segments();
 
-        // For each entry in the map of seen Edges
-        while (iterator.hasNext()) {
+        for (Segment segment: edgeSegments) {
+            if (globalSegments.containsKey(segment)) {
+                // add identifier to the list of identifiers with that segment
+                globalSegments.get(segment).add(identifier);
 
-            final Map.Entry pair = (Map.Entry) iterator.next();
-            final Edge mapEdge = (Edge) pair.getValue();
-            final List<Segment> mapEdgeSegments = mapEdge.asPolyLine().segments();
-
-            if (mapEdgeSegments.containsAll(currentEdgeSegments)) {
-
+            } else {
+                // if it doesn't already exist, then add the segment and list with one identifier
+                Set<Long> identifiers = new HashSet<>();
+                identifiers.add(identifier);
+                globalSegments.put(segment, identifiers);
             }
-
-            if (mapEdgeSegments.contains(currentEdgeSegments)) {
-
-            }
-
-            edges.put(mapEdge.getIdentifier(), mapEdgeSegments);
 
         }
+
+
+
+
+
+
+
         return Optional.empty();
     }
-
 
 }
