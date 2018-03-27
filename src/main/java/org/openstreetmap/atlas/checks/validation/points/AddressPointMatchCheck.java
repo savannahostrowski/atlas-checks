9aa38546632ace949d63dfb5e5d8532551b26950
Savannah Ostrowski <savannahostrowski@gmail.com>
2018-03-27 09:11:50 -0700

    Removed unused import

--- src/main/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheck.java
+++ src/main/java/org/openstreetmap/atlas/checks/validation/points/AddressPointMatchCheck.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 import org.openstreetmap.atlas.checks.base.BaseCheck;
